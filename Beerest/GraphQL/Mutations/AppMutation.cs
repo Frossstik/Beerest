45ca893d7b5dd1b8e5d7bc5d7f4da0080bdcf003
Frossstik <lehapoh@gmail.com>
2024-11-06 04:14:07 +0300

    Reworked

--- Beerest/GraphQL/Mutations/AppMutation.cs
+++ Beerest/GraphQL/Mutations/AppMutation.cs
@@ -3,6 +3,8 @@ using Beerest.GraphQL.Types;
 using Beerest.Interfaces;
 using Beerest.Mapping.DTO;
 using Beerest.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace Beerest.GraphQL.Mutations
 {
@@ -24,67 +26,97 @@ namespace Beerest.GraphQL.Mutations
         public async Task<BarsType> CreateBar(BarsDto input)
         {
             var bar = _mapper.Map<Bars>(input);
+            var beers = new List<Beers>();
+            foreach (var beerId in input.BeerIds)
+            {
+                var beer = await _beersRepository.GetByIdAsync(beerId);
+                if (beer == null)
+                {
+                    throw new Exception($"Пива под номером {beerId} нету!");
+                }
+                beers.Add(beer);
+            }
+            bar.Beers = beers;
             await _barsRepository.CreateAsync(bar);
             return _mapper.Map<BarsType>(bar);
         }
 
-        public async Task<BarsType> UpdateBar(int id, BarsDto input)
+        public async Task<BarsType> UpdateBar(int id, BarsDto input, [Service] AppDbContext context)
         {
-            var bar = await _barsRepository.GetByIdAsync(id);
-            if (bar == null) throw new Exception("Bar not found");
+            var bar = await context.bars.FindAsync(id);
+            if (bar == null) throw new Exception("Бар не найден!");
             _mapper.Map(input, bar);
-            await _barsRepository.UpdateAsync(bar);
+            await context.SaveChangesAsync();
             return _mapper.Map<BarsType>(bar);
         }
 
-        // Delete Bar
-        public async Task<bool> DeleteBar(int id)
+        public async Task<bool> DeleteBar(int id, [Service] AppDbContext context)
         {
-            await _barsRepository.DeleteAsync(id);
+            var bar = await context.bars.FindAsync(id);
+            if (bar == null) return false;
+            context.bars.Remove(bar);
+            await context.SaveChangesAsync();
             return true;
         }
 
-        public async Task<BeersType> CreateBeer(BeersDto input)
+        public async Task<BeersType> CreateBeer(BeersDto input, [Service] AppDbContext context)
         {
             var beer = _mapper.Map<Beers>(input);
-            await _beersRepository.CreateAsync(beer);
+            await context.beers.AddAsync(beer);
+            await context.SaveChangesAsync();
             return _mapper.Map<BeersType>(beer);
         }
 
-        public async Task<BeersType> UpdateBeer(int id, BeersDto input)
+        public async Task<BeersType> UpdateBeer(int id, BeersDto input, [Service] AppDbContext context)
         {
-            var beer = await _beersRepository.GetByIdAsync(id);
-            if (beer == null) throw new Exception("Beer not found");
+            var beer = await context.beers.FindAsync(id);
+            if (beer == null) throw new Exception("Пивас не найден!");
             _mapper.Map(input, beer);
-            await _beersRepository.UpdateAsync(beer);
+            await context.SaveChangesAsync();
             return _mapper.Map<BeersType>(beer);
         }
 
-        public async Task<bool> DeleteBeer(int id)
+        public async Task<bool> DeleteBeer(int id, [Service] AppDbContext context)
         {
-            await _beersRepository.DeleteAsync(id);
+            var beer = await context.beers.FindAsync(id);
+            if (beer == null) return false;
+            context.beers.Remove(beer);
+            await context.SaveChangesAsync();
             return true;
         }
 
         public async Task<PersonsType> CreatePerson(PersonsDto input)
         {
             var person = _mapper.Map<Persons>(input);
+
+            if (input.BarId.HasValue)
+            {
+                var bar = await _barsRepository.GetByIdAsync(input.BarId.Value);
+                if (bar == null)
+                {
+                    throw new Exception("Бар не найден!");
+                }
+                person.Bar = bar;
+            }
             await _personsRepository.CreateAsync(person);
             return _mapper.Map<PersonsType>(person);
         }
 
-        public async Task<PersonsType> UpdatePerson(int id, PersonsDto input)
+        public async Task<PersonsType> UpdatePerson(int id, PersonsDto input, [Service] AppDbContext context)
         {
-            var person = await _personsRepository.GetByIdAsync(id);
-            if (person == null) throw new Exception("Person not found");
+            var person = await context.persons.FindAsync(id);
+            if (person == null) throw new Exception("Челик не найден!");
             _mapper.Map(input, person);
-            await _personsRepository.UpdateAsync(person);
+            await context.SaveChangesAsync();
             return _mapper.Map<PersonsType>(person);
         }
 
-        public async Task<bool> DeletePerson(int id)
+        public async Task<bool> DeletePerson(int id, [Service] AppDbContext context)
         {
-            await _personsRepository.DeleteAsync(id);
+            var person = await context.persons.FindAsync(id);
+            if (person == null) return false;
+            context.persons.Remove(person);
+            await context.SaveChangesAsync();
             return true;
         }
     }
